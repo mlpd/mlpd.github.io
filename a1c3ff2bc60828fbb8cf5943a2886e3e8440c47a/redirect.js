e3cecda2f75545524ae6a00dd5eeebdc74a70476
mlpd <milap88+git@gmail.com>
2022-02-05 11:42:28 -0600

    Update redirect.js

--- a1c3ff2bc60828fbb8cf5943a2886e3e8440c47a/redirect.js
+++ a1c3ff2bc60828fbb8cf5943a2886e3e8440c47a/redirect.js
@@ -1,3 +1,3 @@
-if (document.referrer != "http://mlpd.github.io") {
-    window.location.href = "http://mlpd.github.io";
+if (document.referrer !== "https://mlpd.github.io") {
+    window.location.href = "https://mlpd.github.io";
 }
